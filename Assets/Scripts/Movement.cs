40861c6170eb7384f3c73db07a45253b611efb5b
Jayden-digipen <1494644@edu.burnabyschools.ca>
2024-04-18 11:45:37 -0700

    ,mhhjkjk

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,23 +5,68 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     //Created by Jayden, dont edit it
-    int carSpeed = 5;
+    int carSpeed = 1;
+    int accelCar = 1;
+    int maxSpeed = 50;
+   
+    Vector3 forward = new Vector3(0,0,1);
+    Vector3 backward = new Vector3(0, 0, -1);
 
+    Vector3 right = new Vector3(1,0,0);
+    Vector3 left = new Vector3(-1, 0, 0);
+    
+    Vector3 playerMove;
+
+    
+
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
+   
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CarMovement();
     }
 
     private void CarMovement()
     {
-        float sideMove = Input.GetAxis("Side") * carSpeed * Time.deltaTime;
-        float frontMove = Input.GetAxis("Front") * carSpeed * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+
+
+            rb.MovePosition(playerMove.position, forward, 0, 0);
+            
+        }
+
+       
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+
+            rb.AddForce(10, 0, 0);
+
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W) & accelCar < maxSpeed)
+        {
+
+            rb.AddForce(0, carSpeed, 0);
+
+
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S) & accelCar < maxSpeed)
+        {
+            rb.AddForce(0, -10, 0);
+
+
+
+        }
+
     }
 }
